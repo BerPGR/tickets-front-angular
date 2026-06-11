bbd46cd312c9a158631cd14df9d6b8d730ad48d8
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2026-06-11 14:38:21 -0300

    fetch de teams

--- src/app/pages/newticket/newtickt.component.ts
+++ src/app/pages/newticket/newtickt.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TeamsService } from '../../services/teams.service';
+import { Team } from '../../class/team';
 
 @Component({
   selector: 'app-newtickt.component',
@@ -6,4 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './newticket.component.html',
   styleUrl: './newtickt.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent implements OnInit {
+  teams: Team[] = []
+  
+  constructor(private tService: TeamsService) {}
+
+  ngOnInit(): void {
+    this.tService.fetchTeams().subscribe({
+      next: (valor) => {
+        this.teams = [...valor]
+      },
+      error: erro => console.log(erro)
+    })
+  }
+
+
+}
--- src/app/services/teams.service.ts
+++ src/app/services/teams.service.ts
@@ -1,7 +1,13 @@
 import { Service, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Team } from '../class/team';
 
 @Service()
 export class TeamsService {
     http = inject(HttpClient)
+
+    fetchTeams(): Observable<Team[]> {
+        return this.http.get<Team[]>("http://localhost:8000/teams")
+    }
 }
