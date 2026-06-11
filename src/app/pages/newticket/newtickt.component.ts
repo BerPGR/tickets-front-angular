0e98a1ae428377c6e117cf57f2efa710e8ccf85f
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2026-06-11 15:53:05 -0300

    pagina inicial

--- src/app/pages/newticket/newtickt.component.ts
+++ src/app/pages/newticket/newtickt.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { TeamsService } from '../../services/teams.service';
 import { Team } from '../../class/team';
+import { CardModule } from 'primeng/card'
+import { ButtonModule } from 'primeng/button'
+import { SelectModule } from 'primeng/select'
+import { FormsModule } from "@angular/forms"
+import { CommonModule } from '@angular/common';
+import { FloatLabelModule } from 'primeng/floatlabel'
 
 @Component({
   selector: 'app-newtickt.component',
-  imports: [],
+  imports: [CardModule, ButtonModule, FloatLabelModule, FormsModule, CommonModule, SelectModule],
   templateUrl: './newticket.component.html',
   styleUrl: './newtickt.component.css',
 })
 export class NewTicketComponent implements OnInit {
   teams: Team[] = []
+  selectedTeam = null 
   
   constructor(private tService: TeamsService) {}
 
@@ -22,5 +29,8 @@ export class NewTicketComponent implements OnInit {
     })
   }
 
+  createTicket() {
+    console.log(this.selectedTeam)
+  }
 
 }
