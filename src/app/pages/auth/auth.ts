413967c255aae76278d110444539ac105bee72c1
BerPGR <bernardopgr01@gmail.com>
2026-06-23 16:02:28 -0300

    fix login

--- src/app/pages/auth/auth.ts
+++ src/app/pages/auth/auth.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider'
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -14,6 +16,8 @@ import { InputTextModule } from 'primeng/inputtext';
 })
 export class Auth {
   authForm: FormGroup
+  router = inject(Router)
+  authService = inject(AuthService)
 
   constructor() {
     this.authForm = new FormGroup({
@@ -24,6 +28,10 @@ export class Auth {
 
   login() {
     this.authForm.markAllAsTouched()
-    console.log(this.authForm.value)
+    this.authService.login(this.authForm.value.email, this.authForm.value.password).subscribe({
+      next: (_) => {
+        this.router.navigate(['/'])
+      }
+    })
   }
 }
